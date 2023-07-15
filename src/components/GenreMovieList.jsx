c975b41d454821edf912e259cd9881fd39ec37bf
yusufobr <yousarsif80@gmail.com>
2023-07-16 00:20:24 +0100

    edite padding

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -4,12 +4,12 @@ import MovieList from "./MovieList";
 
 function GenreMovieList() {
   return (
-    <div className="container max-w-screen-xl mx-auto">
+    <div className="container max-w-screen-xl mx-auto my-4 mb-20">
       {genere.map(
         (item, index) =>
           index <= 4 && (
-            <div className="p-8">
-              <h2 className="text-lg text-white font-semibold mb-4">{item.name}</h2>
+            <div className="p-2 md:p-8">
+              <h2 className="text-lg text-white ml-3 font-semibold mb-4">{item.name}</h2>
               <MovieList id={item.id} />
             </div>
           )
